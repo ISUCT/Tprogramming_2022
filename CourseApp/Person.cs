a2cd01c1fbfec1eaf6773ade6c8edc5a9409c5fb
1Waldemar1 <dantedmc.5@mail.ru>
2022-09-12 16:50:06 +0300

    fix

--- CourseApp/Person.cs
+++ CourseApp/Person.cs
@@ -29,7 +29,7 @@ namespace CourseApp
 
             set
             {
-                if (value > 0 && value < 100)
+                if (value > 0 && value <= 110)
                 {
                     this.age = value;
                 }
@@ -45,7 +45,7 @@ namespace CourseApp
 
             set
             {
-                if (value > 45 && value < 240)
+                if (value >= 45 && value <= 240)
                 {
                     this.height = value;
                 }
@@ -61,7 +61,7 @@ namespace CourseApp
 
             set
             {
-                if (value > 0.3 && value < 300)
+                if (value >= 0.3 && value <= 300)
                 {
                     this.weight = value;
                 }
@@ -70,45 +70,65 @@ namespace CourseApp
 
         public void Midlle_Age()
         {
-            if (age < 18)
+            if (age > 0 && age < 18)
             {
                 System.Console.WriteLine("Are you a child;");
             }
-            else if (age == 18 || age < 45)
+            else if (age == 0)
+            {
+                System.Console.WriteLine("Input error in age;");
+            }
+            else if (age >= 18 && age <= 44)
             {
                 System.Console.WriteLine("You're a young man;");
             }
-            else if (age >= 59)
+            else if (age >= 45 && age <= 59)
             {
                 System.Console.WriteLine("You're middle-aged;");
             }
-            else if (age <= 100)
+            else if (age >= 60 && age <= 74)
             {
                 System.Console.WriteLine("You are an elderly man;");
+            }
+            else if (age >= 75 && age <= 89)
+            {
+                System.Console.WriteLine("Are you an old man;");
+            }
+            else if (age >= 90)
+            {
+                System.Console.WriteLine("Are you a long-lived person;");
             }
         }
 
         public void Average_Height()
         {
-            if (height < 177)
+            if (height >= 45 && height < 177)
             {
                 System.Console.WriteLine("Your height is below average in Russia;");
             }
-            else if (height >= 177)
+            else if (height >= 177 && height <= 240)
             {
                 System.Console.WriteLine("Your height is above average in Russia;");
+            }
+            else if (height == 0)
+            {
+                System.Console.WriteLine("Input error in height;");
             }
         }
 
         public void Average_Weight()
         {
-            if (height < 80)
+            if (weight >= 0.3 && weight <= 80)
             {
                 System.Console.WriteLine("Your weight is below average in Russia.");
             }
-            else if (height > 80)
+            else if (weight > 80 && weight <= 300)
             {
                 System.Console.WriteLine("Your weight is above average in Russia.");
+            }
+            else if (weight == 0)
+            {
+                System.Console.WriteLine("Input error in weight;");
             }
         }
 
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -6,15 +6,15 @@ public class Program
         public static void Main(string[] args)
         {
             Person person = new Person(45);
-            person.Height = 177;
+            person.Height = 176;
             person.Weight = 74;
             person.Show();
-            System.Console.WriteLine("Based on the results of the entered data");
-            person.Midlle_Age();
-            person.Average_Height();
-            person.Average_Weight();
             System.Console.WriteLine();
-            Person person1 = new Person(20, 190, 91);
+            Person person1 = new Person(20, 177, 99);
             person1.Show();
+            System.Console.WriteLine("Based on the results of the entered data");
+            person1.Midlle_Age();
+            person1.Average_Height();
+            person1.Average_Weight();
         }
 }
