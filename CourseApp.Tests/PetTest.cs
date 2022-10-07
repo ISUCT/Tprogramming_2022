4ee98c3a3ee9bf052ffa3d449012b28655b31b47
ksyukornilich <ksyu_kornilich@mail.ru>
2022-10-07 19:04:42 +0300

    first test

---
+++ CourseApp.Tests/PetTest.cs
@@ -0,0 +1,22 @@
+﻿namespace CourseApp.Tests
+{
+    using CourseApp.Entities;
+    using Xunit;
+
+    public class PetTest
+    {
+        [Fact]
+        public void BirdTest()
+        {
+            var bird = new Bird("Кеша", "зелёный", 5);
+            var expectedText = @"  зелёный попугай Кеша
+           \\
+   \\      (o>
+   (o>     //\
+  _(()_____V_/_____
+   ||      ||
+           ||";
+            Assert.Equal(expectedText, bird.GetInfo());
+        }
+    }
+}
