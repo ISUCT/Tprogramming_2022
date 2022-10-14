cd8fdb4565b07ea265e7d980a564c16e903ed3f7
NikitaZhulenkov <92643849+NikitaZhulenkov@users.noreply.github.com>
2022-10-14 09:49:51 +0300

    Тесты ООП

---
+++ CourseApp.Tests/Auto_Tests.cs
@@ -0,0 +1,53 @@
+﻿namespace CourseApp.Tests
+{
+    using Xunit;
+
+    public class Auto_Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TestConstructor()
+        {
+            var car = new Auto();
+            Assert.Equal("Car", car.Name);
+            Assert.Equal(45, car.Speed);
+            Assert.Equal(80, car.LoadingCapacity);
+        }
+
+        [Fact]
+        public void TestOk()
+        {
+            var car = new Auto();
+            car.Speed = 50;
+            Assert.Equal(50, car.Speed);
+        }
+
+        [Fact]
+        public void TestBadSpeedLessThanZero()
+        {
+            var car = new Auto();
+            car.Speed = -26;
+            Assert.Equal(35, car.Speed);
+        }
+
+        [Fact]
+        public void TestRide()
+        {
+        }
+
+        [Fact]
+        public void TestStop()
+        {
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+        }
+    }
+}
