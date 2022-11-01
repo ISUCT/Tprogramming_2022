a39869ef49e659f55830ada735e168d36b0c9f3e
Violettam88 <violettam88@yandex.ru>
2022-11-01 15:16:51 +0300

    done task 2

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -1,18 +1,13 @@
-﻿namespace CourseApp.Task_one
+﻿namespace CourseApp.Task_two
 {
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            Country one = new Country();
-            Country two = new Country("Brazil", 121321, 3213);
-            one.Info();
-            two.Info();
-            one.ChangeArea();
-            two.Population = 500;
-            two.Population = -500;
-            one.Info();
+            England test = new England();
+            test.Info();
+            test.ChangeArea();
+            test.ChangePopulation();
         }
     }
 }
