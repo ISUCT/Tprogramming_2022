34fdb733da6457509fdc424dc246e968c8081df2
Gv1don <crgvidonlol@gmail.com>
2022-11-10 19:46:24 +0300

    Create class for creating player

---
+++ CourseApp/RPGSaga/CreatePlayers.cs
@@ -0,0 +1,26 @@
+﻿namespace CourseApp.RPGSaga
+{
+    using System;
+    using CourseApp.Class;
+
+    public static class CreatePlayers
+    {
+        private static Random _random = new ();
+
+        public static PlayerTypes GetHeroType()
+        {
+                var type = _random.Next(0, 3);
+                switch (type)
+                {
+                    case 0:
+                        return PlayerTypes.Knight;
+                    case 1:
+                        return PlayerTypes.Wizard;
+                    case 2:
+                        return PlayerTypes.Archer;
+                    default:
+                        return PlayerTypes.NotType;
+                }
+        }
+    }
+}
