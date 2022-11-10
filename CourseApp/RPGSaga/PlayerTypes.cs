2fcd3ded70dc3937e9377a98729770d41313ef0e
Gv1don <crgvidonlol@gmail.com>
2022-11-10 19:24:25 +0300

    Add enum with player types

---
+++ CourseApp/RPGSaga/PlayerTypes.cs
@@ -0,0 +1,28 @@
+﻿namespace CourseApp.Class
+{
+    /// <summary>
+    /// Types of actions.
+    /// </summary>
+    public enum PlayerTypes
+    {
+        /// <summary>
+        /// No type.
+        /// </summary>
+        NotTypes,
+
+        /// <summary>
+        /// Type for Knight.
+        /// </summary>
+        Knight,
+
+        /// <summary>
+        /// Type for Archer.
+        /// </summary>
+        Archer,
+
+        /// <summary>
+        /// Types for Wizard.
+        /// </summary>
+        Wizard,
+    }
+}
