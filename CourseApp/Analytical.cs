efd44ce939d21e7061850f666133e7b69bd92b75
markkket <92202536+markkket@users.noreply.github.com>
2022-12-03 19:04:26 +0300

    Add files via upload

---
+++ CourseApp/Analytical.cs
@@ -0,0 +1,31 @@
+﻿namespace CourseApp;
+
+using System;
+
+public class Analytical : Document
+{
+    public Analytical(string name, string type, double size)
+        : base(name, type, size)
+    {
+    }
+
+    public override string DocumentSize()
+    {
+        return $"{Size} Мб ";
+    }
+
+    public override string Way()
+    {
+        return "C:/work/documents";
+    }
+
+    public override string NameOfEl()
+    {
+        return "AnalyticalDocument: ";
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}; {Size} Мб; {Type}";
+    }
+}
---
+++ CourseApp/Document.cs
@@ -0,0 +1,39 @@
+﻿namespace CourseApp;
+
+using System;
+
+public abstract class Document
+{
+    public Document(string name, string type, double size)
+    {
+        this.Name = name;
+        this.Type = type;
+        this.Size = size;
+    }
+
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public double Size { get; set; }
+
+    public void Print()
+    {
+        Console.Write($"Название документа: {Name}  Тип документа: {Type}  Размер документа: {Size} Мб");
+    }
+
+    public virtual string DocumentSize()
+    {
+        return "200 Мб";
+    }
+
+    public virtual string Way()
+    {
+        return "sss";
+    }
+
+    public virtual string NameOfEl()
+    {
+        return "3";
+    }
+}
